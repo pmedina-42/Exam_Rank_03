93e8a42560bea2b325170d9cecb6f7218f54d9cb
Patricia Medina anglada <pmedina-@c1r7s1.42madrid.com>
2021-06-29 22:46:44 +0200

    micro corregido

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -3,69 +3,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t	len(char *s)
+size_t		len(char *s)
 {
-	int i = 0;
-	while (s[i])
-		i++;
-	return (i);
+    int    i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
 
-int	errror(char *s)
+int        errror(char *s)
 {
-	write(1, s, len(s));
-	return (1);
+    write(1, s, len(s));
+    return (1);
 }
 
-int checkpos(float start_x, float start_y, float rwidth, float rheigth, float posx, float posy)
+int        checkpos(float x, float y, float start_x, float start_y, float rwidth, float rheight)
 {
-	if (posx < start_x || posx > start_x + rwidth || posy < start_y || posy > start_y + rheigth)
-		return (0);
-	else if ((posx - start_x) < 1 || (start_x + rwidth - posx) < 1 || (posy - start_y) < 1 || (start_y + rheigth - posy) < 1)
-		return (1);
-	return (2);
+    if (x < start_x || x > start_x + rwidth || y < start_y || y > start_y + rheight)
+        return (0);
+    else if (x - start_x < 1 || start_x + rwidth - x < 1 || y - start_y < 1 || start_y + rheight - y < 1)
+        return (1);
+    return (2);
 }
 
-int	main(int argc, char **argv)
+int        main(int argc, char *argv[])
 {
-	FILE *file;
-	char *paper, bg_color, filling, type;
-	int x, y, total_width, total_heigth, pos, read;
-	float start_x, start_y, rwidth, rheigth;
+    FILE    *file;
+    char    *paper, bg_color, type, filling;
+    int        read, pos, x, y, total_width, total_height;
+    float    start_x, start_y, rwidth, rheight;
 
-	if (argc != 2)
-		return (errror("Error :argument\n"));
-	if (!(file = fopen(argv[1], "r")) || (fscanf(file, "%i %i %c\n", &total_width, &total_heigth, &bg_color) != 3) || total_width <= 0 || total_heigth <= 0 || total_width > 300 || total_heigth > 300 || (!(paper = (char *)malloc(sizeof(char) * total_heigth * total_width))))
-		return (errror("Error: Operation file corrupted\n"));
-	memset(paper, bg_color, total_width * total_heigth);
-	while((read = fscanf(file, "%c %f %f %f %f %c\n", &type, &start_x, &start_y, &rwidth, &rheigth, &filling)) == 6)
-	{
-		if ((type != 'R' && type != 'r') || rheigth < 1 || rwidth < 1)
-			break;
-		x = -1;
-		while (++x < total_width)
-		{
-			y = -1;
-			while (++y < total_heigth)
-			{
-				pos = checkpos(start_x, start_y, rwidth, rheigth, x, y);
-				if ((pos == 1 && type == 'R') || pos == 0)
-					paper[y * total_width + x] = filling;
-			}
-		}
-	}
-	if (read != -1)
-	{
-		free(paper);
-		return (errror("Error: Operation file corrupted\n"));
-	}
-	y = -1;
-	while (++y < total_heigth)
-	{
-		write(1, paper + y * total_width, total_width);
-		write(1, "\n", 1);
-	}
-	free(paper);
-	fclose(file);
-	return (0);
+    if (argc != 2)
+        return (errror("Error: argument\n"));
+    if (!(file = fopen(argv[1], "r")) ||
+        (fscanf(file, "%i %i %c\n", &total_width, &total_height, &bg_color) != 3) ||
+        total_width <= 0 || total_width > 300 || total_height <= 0 || total_height > 300 ||
+        (!(paper = (char *)malloc(sizeof(char) * (total_width * total_height)))))
+        return (errror("Error: Operation file corrupted\n"));
+    memset(paper, bg_color, total_width * total_height);
+    while ((read = fscanf(file, "%c %f %f %f %f %c\n", &type, &start_x, &start_y, &rwidth, &rheight, &filling)) == 6)
+    {
+        if (!(rwidth > 0 && rheight > 0) || !(type == 'R' || type == 'r'))
+            break ;
+        x = -1;
+        while (++x < total_width)
+        {
+            y = -1;
+            while (++y < total_height)
+            {
+                pos = checkpos((float)x, (float)y, start_x, start_y, rwidth, rheight);
+                if (pos == 1 || (pos == 2 && type == 'R'))
+                    paper[y * total_width + x] = filling;
+            }
+        }
+    }
+    if (read != -1)
+    {
+        free(paper);
+        return (errror("Error: Operation file corrupted\n"));
+    }
+    y = -1;
+    while (++y < total_height)
+    {
+        write(1, paper + y * total_width, total_width);
+        write(1, "\n", 1);
+    }
+    free(paper);
+    fclose(file);
+    return (0);
 }
